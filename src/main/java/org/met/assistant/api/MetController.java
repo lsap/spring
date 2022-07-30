47ac7361019df5580dd3ba55f54325bce23409b4
serhii <a@b.co>
2022-07-30 18:10:52 +0300

    Adjusted a route.

--- src/main/java/org/met/assistant/api/MetController.java
+++ src/main/java/org/met/assistant/api/MetController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MetController {
 
-    @GetMapping("/customerportal")
+    @GetMapping("/tme")
     public UserResponse getUser() {
         return new UserResponse();
     }
