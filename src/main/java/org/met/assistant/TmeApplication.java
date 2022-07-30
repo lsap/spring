e7e0503f961d88afa04884ca810af938395ff026
serhii <a@b.co>
2022-07-30 10:46:00 +0300

    Initial commit

---
+++ src/main/java/org/met/assistant/TmeApplication.java
@@ -0,0 +1,13 @@
+package org.met.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/met/assistant/TmeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.met.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
