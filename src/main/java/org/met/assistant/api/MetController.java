5a4f7bc59d6a0508e4f7429e1c7186cfd6539efe
serhii <a@b.co>
2022-07-30 10:56:06 +0300

    The first JSON.

--- src/main/java/org/met/assistant/api/MetController.java
+++ src/main/java/org/met/assistant/api/MetController.java
@@ -1,7 +1,15 @@
 package org.met.assistant.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class MetController {
+
+    @GetMapping("/customerportal")
+    public String getUser() {
+        return "{\"success\":1}";
+    }
 }
