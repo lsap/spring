e334b327e0b19d278f7a918b77ed2c6091f0c6e5
serhii <a@b.co>
2022-07-30 16:19:50 +0300

    Empire strikes back.

--- src/main/java/org/met/assistant/api/MetController.java
+++ src/main/java/org/met/assistant/api/MetController.java
@@ -1,6 +1,7 @@
 package org.met.assistant.api;
 
 import lombok.RequiredArgsConstructor;
+import org.met.assistant.model.UserResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MetController {
 
     @GetMapping("/customerportal")
-    public String getUser() {
-        return "{\"success\":1}";
+    public UserResponse getUser() {
+        return new UserResponse();
     }
 }
---
+++ src/main/java/org/met/assistant/model/UserResponse.java
@@ -0,0 +1,18 @@
+package org.met.assistant.model;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserResponse {
+    private boolean verificationRequired = true;
+    private String clientRedirectionUrl = "https://www.toyota.co.uk";
+    private String marketing = "https://www.toyota.co.uk/#/my-toyota";
+    private String profile = "https://www.toyota.co.uk/#/publish/my_toyota_userprofile";
+    private String reponsiveIframe = "https://www.toyota.co.uk/scripts/responsiveiframe";
+    private String ppm = "https://www.toyota.co.uk/scripts/postalPostMessage";
+    private boolean isVOT2Enabled = true;
+}
